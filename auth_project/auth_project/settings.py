c7050f66de7770856b3b0f9da14621b74727632b
Art3mis <pyatt.eve@gmail.com>
2025-08-30 16:38:18 +0000

    there

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7cz&s3%7ozj#xuaw7#hl^k_iop0f6%fcj(y&wve4znv%3lf)s=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://*.hackclub.com']
 
 # Application definition
 
